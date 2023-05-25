b6ae6db7c39a289c0d523decfd2a387162f9c3de
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-05-24 20:03:35 -0400

    Create and add api helper for parallelum and catfacts api

--- Assignment-1-API/CatFacts_Helper.swift
+++ Assignment-1-API/CatFacts_Helper.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class CatFacts_Helper {
+    private static let urlString = "https://meowfacts.herokuapp.com/"
+    
+    public static func fetch() async -> Any {
+        let catFactsUrl =  URL(string: urlString)!
+        let (data, _) = try! await URLSession.shared.data(from: catFactsUrl)
+        
+        let jsonObject = try! JSONSerialization.jsonObject(with: data)
+        return jsonObject
+    }
+}
--- Assignment-1-API/ParallelumAPI_Helper.swift
+++ Assignment-1-API/ParallelumAPI_Helper.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class ParallelumAPI_Helper {
+    private static let urlString = "https://meowfacts.herokuapp.com/"
+    
+    public static func fetch() async -> Any {
+        let parallelumUrl =  URL(string: urlString)!
+        let (data, _) = try! await URLSession.shared.data(from: parallelumUrl)
+        
+        let jsonObject = try! JSONSerialization.jsonObject(with: data)
+        return jsonObject
+    }
+}
